ba94d84389123c707d27a3dae641524a8424a253
Live fish is worried, Dead Fish is happy <461437874@qq.com>
2023-08-13 15:19:53 +0800

    [美化|修改]美化启动界面；优化wake事件处理

--- __core/controller/index.js
+++ __core/controller/index.js
@@ -7,7 +7,9 @@ class KtRoot extends React.Component {
             appList: {},
             isShow: false,
             appBaseWidth: 0,
-            appBaseHeight: 0
+            appBaseHeight: 0,
+            appCenterX: 0,
+            appCenterY: 0
         }
 
         this.mainBodyRef = React.createRef()
@@ -34,13 +36,43 @@ class KtRoot extends React.Component {
         });
 
         // 监听window的出现和消失事件
-        kt.on.windowShow(async (e, args) => {
-            this.activeAudioRef.current.play()
-            await this.setPos()
+        // kt.on.windowShow(async (e, args) => {
+        //     await this.stopAllAudio()
+        //     this.activeAudioRef.current.play()
+        //     await this.setPos()
+        //     await this.doShow()
+        // })
+
+        // kt.on.windowHide(async (e, args) => {
+        //     await this.doHide()
+        // })
+
+        kt.on.windowWake(async (e, args) => {
+            if (this.state.isShow) {
+                await this.doHide()
+                return
+            }
+
             await this.doShow()
         })
     }
 
+    // 停掉所有正在播放的音效
+    async stopAllAudio() {
+        const list = [
+            this.clickAudioRef.current,
+            this.activeAudioRef.current,
+            this.inActiveAudioRef.current
+        ]
+
+        for (let i = 0; i < list.length; i++) {
+            list[i].pause()
+            list[i].currentTime = 0
+        }
+        return
+    }
+
+    // 根据屏幕大小，设置好基础参数值与窗口大小
     async setPos() {
         const cursorPoint = (await await kt.eScreen.getCursorScreenPoint()).result
         const screenInfo = (await kt.eScreen.getDisplayMatching({
@@ -66,7 +98,9 @@ class KtRoot extends React.Component {
         // 设置app框的大小
         this.setState({
             appBaseWidth: screenInfo.bounds.width / 10,
-            appBaseHeight: screenInfo.bounds.height / 10
+            appBaseHeight: screenInfo.bounds.height / 10,
+            appCenterX: screenInfo.bounds.width / 2,
+            appCenterY: screenInfo.bounds.height / 2
         })
     }
 
@@ -76,6 +110,7 @@ class KtRoot extends React.Component {
             height: '100vh',
             display: 'flex',
             flexDirection: 'column',
+            backgroundColor: '#1d1d1dcc',
             flexWrap: 'wrap',
             alignItems: 'center',
             justifyContent: 'center',
@@ -87,8 +122,8 @@ class KtRoot extends React.Component {
     contentStyle() {
         const colors = window.KtReactComponents.themes[window.KtReactComponents.usingTheme]
         return {
-            width: '80%',
-            height: '60%',
+            width: '100%',
+            height: '100%',
             // flexGrow: 1,
             zIndex: 1,
             display: 'flex',
@@ -99,17 +134,22 @@ class KtRoot extends React.Component {
     }
 
     async doHide() {
+        await this.stopAllAudio()
         this.inActiveAudioRef.current.play()
         this.setState({
             isShow: false
         }, async () => {
             setTimeout(async () => {
                 await kt.window.hide()
-            }, 200)  
-        }) 
+            }, 200)
+        })
     }
 
     async doShow() {
+        await this.stopAllAudio()
+        this.activeAudioRef.current.play()
+        await this.setPos()
+        await kt.window.show()
         this.setState({
             isShow: true
         }, () => {
@@ -118,7 +158,7 @@ class KtRoot extends React.Component {
     }
 
     async clickMask() {
-        await this.doHide() 
+        await this.doHide()
     }
 
     async openApp(appDirName) {
@@ -130,70 +170,64 @@ class KtRoot extends React.Component {
 
     render() {
         const colors = window.KtReactComponents.themes[window.KtReactComponents.usingTheme]
+        const isHor = this.state.appBaseWidth > this.state.appBaseHeight
         return (
-            <div style={this.getStyle()} ref={this.mainBodyRef}
+            <div style={this.getStyle()}
+                ref={this.mainBodyRef}
                 onClick={() => { this.clickMask() }}
                 className={`${this.state.isShow ? 'fade-in' : 'fade-out'}`}
             >
                 <GlobalHandler hotUpdate={true} />
                 <div
                     className={`${this.state.isShow ? 'slide-right' : 'slide-left'} left-top-title`}>
-                    - Capybara -
+                    - FishMint -
+                </div>
+                <div className={`left-top-icon ${this.state.isShow ? 'slide-right' : 'slide-left'}`}>
+                    <img src="./FishMint.png" />
                 </div>
                 <div style={this.contentStyle()}>
-                    {/* 内置App列表、选项 */}
                     <div style={{
                         width: '100%',
                         height: '100%',
-                        display: 'flex',
-                        flexWrap: 'wrap',
-                        justifyContent: 'center',
-                        // border: '1px solid #eeeeee11',
-                        // borderRadius: '5px 5px 5px 5px',
-                        color: '#fff',
-                        // backgroundColor: '#333333ee',
-                        padding: '20px 20px 20px 20px'
-                    }} className={`click-through`}>
-                        {/* 第一行 */}
-                        <div style={{
-                            width: '100%',
-                            display: 'flex'
-                        }}>
-                            {/* 填满剩下的 */}
-                            <div style={{
-                                flexGrow: 1
-                            }}></div>
-                            <div 
-                                className={`app-container sunshine-box-shadow ${this.state.isShow ? 'slide-down' : 'slide-up'}`}
-                                style={{
-                                    width: this.state.appBaseWidth * 4 + 'px',
-                                    height: this.state.appBaseHeight * 3 + 'px',
-                                }} onClick={(e) => { e.stopPropagation(); this.openApp("__core/appList") }}>
-                                挂件库
-                            </div>
+                        display: 'flex'
+                    }}>
+                        <div
+                            className={`app-container sunshine-box-shadow ${this.state.isShow ? 'slide-down' : 'slide-up'}`}
+                            style={{
+                                width: this.state.appBaseWidth * (isHor ? 1 : 2) + 'px',
+                                height: this.state.appBaseWidth * (isHor ? 1 : 2) + 'px',
+                                position: 'absolute',
+                                left: `${this.state.appCenterX - this.state.appBaseWidth * 2}px`,
+                                top: `${this.state.appCenterY - this.state.appBaseHeight * (isHor ? 3 : 2)}px`,
+                                fontSize: `${this.state.appBaseWidth * (isHor ? 0.2 : 0.5)}px`
+                            }} onClick={(e) => { e.stopPropagation(); this.openApp("__core/appList") }}>
+                            挂件库
                         </div>
-                        {/* 第二行 */}
-                        <div style={{
-                            width: '100%',
-                            display: 'box'
-                        }}>
-                            <div
-                                className={`app-container sunshine-box-shadow ${this.state.isShow ? 'slide-down' : 'slide-up'}`}
-                                style={{
-                                    width: this.state.appBaseWidth * 3 + 'px',
-                                    height: this.state.appBaseHeight * 4 + 'px',
-                                    position: 'absolute',
-                                    bottom: this.state.appBaseHeight * 2 + 'px'
-                                }} onClick={() => { this.openApp("__core/setting") }}>
-                                设置
-                            </div>
+
+                        <div
+                            className={`app-container sunshine-box-shadow ${this.state.isShow ? 'slide-down' : 'slide-up'}`}
+                            style={{
+                                width: this.state.appBaseWidth * (isHor ? 1 : 2) + 'px',
+                                height: this.state.appBaseWidth * (isHor ? 1 : 2) + 'px',
+                                position: 'absolute',
+                                top: this.state.appCenterY + this.state.appBaseHeight * (isHor ? 1 : 1) + 'px',
+                                left: this.state.appCenterX - this.state.appBaseWidth * (isHor ? 3 : 4) + "px",
+                                fontSize: `${this.state.appBaseWidth * (isHor ? 0.2 : 0.5)}px`
+                            }} onClick={() => { this.openApp("__core/setting") }}>
+                            设置
                         </div>
                     </div>
                 </div>
-                <div
+                {/* <div
                     ref={this.maskRef}
                     className="blurred mask"
-                ></div>
+                ></div> */}
+                <div className={`bg-img`}>
+                    <img className={`${this.state.isShow ? 'grow-big' : 'grow-small'}`} 
+                        style={{
+                        width: `${isHor ? '50%' : '90%'}`
+                    }} src="./bg-trans-color.png" />
+                </div>
                 <audio ref={this.clickAudioRef} src="/kt_app/utils/res/audios/click.mp3"></audio>
                 <audio ref={this.activeAudioRef} src="/kt_app/__core/controller/Capybara_Active.mp3"></audio>
                 <audio ref={this.inActiveAudioRef} src="/kt_app/__core/controller/Capybara_InActive.mp3"></audio>
