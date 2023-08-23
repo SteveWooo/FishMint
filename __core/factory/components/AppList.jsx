0dc783aa8be6de00ab935a09fc5ce749942245e8
Live fish is worried, Dead Fish is happy <461437874@qq.com>
2023-08-23 18:03:57 +0800

    [新增]工坊新增配置，新增项目删除功能

--- __core/factory/components/AppList.jsx
+++ __core/factory/components/AppList.jsx
@@ -1,4 +1,5 @@
 const { DragBar, WindowFrame, GlobalHandler } = window.fmComponents
+const { Select, MenuItem, styled } = window.MaterialUI
 
 class AppList extends React.Component {
     constructor(p) {
@@ -6,14 +7,17 @@ class AppList extends React.Component {
         this.state = {
             appList: {},
             activeAppDirName: '',
-            i18n: {}
+            i18n: {},
+            templates: [],
+            templateListStatus: 'hide', // hide show hidding showing
         }
     }
 
     async componentDidMount() {
         const res = await fm.const()
         this.setState({
-            i18n: res.i18n
+            i18n: res.i18n,
+
         })
         this.updateList()
 
@@ -24,8 +28,10 @@ class AppList extends React.Component {
 
     async updateList() {
         const appList = (await fm.controller.getAppList()).appList
+        const templates = (await fm.staticService.getTemplates()).templates
         this.setState({
-            appList: appList
+            appList: appList,
+            templates: templates
         }, () => {
             // for debug
             const app = appList[Object.keys(appList)[0]]
@@ -37,8 +43,41 @@ class AppList extends React.Component {
         this.setState({
             activeAppDirName: appInfo.appDirName
         })
-
         this.props.selectApp && this.props.selectApp(appInfo)
+    }
+
+    // 用模板创建一个app
+    async createAppByTemplate(template) {
+        const res = await fm.dialog.showMessageBox({
+            title: this.state.i18n['capp-createApp'],
+            type: 'warning',
+            message: this.state.i18n['capp-isGoingToCreateAnApp'] + template.name,
+            buttons: ['OK', 'Cancel']
+        })
+        if (res.result !== 0) return 
+
+        console.log('createing')
+        await fm.controller.createApp({
+            template: template.name
+        })
+    }
+
+    async hideTemplateSelectMenu() {
+        this.setState({ templateListStatus: 'hidding' }, () => {
+            setTimeout(() => {
+                if (this.state.templateListStatus !== 'hidding') return 
+                this.setState({ templateListStatus: 'hide' })
+            }, 300)
+        })
+    }
+
+    async showTemplateSelectMenu() {
+        this.setState({ templateListStatus: 'showing' }, () => {
+            setTimeout(() => {
+                if (this.state.templateListStatus !== 'showing') return 
+                this.setState({ templateListStatus: 'show' })
+            }, 300)
+        })
     }
 
     render() {
@@ -50,7 +89,8 @@ class AppList extends React.Component {
                 flexDirection: 'column',
                 justifyContent: 'flex-start',
                 alignItems: 'flex-start',
-                userSelect: 'none'
+                userSelect: 'none',
+                overflowX: 'hidden'
             }}>
                 {/* 列表标题 */}
                 <div style={{
@@ -85,7 +125,8 @@ class AppList extends React.Component {
                     {
                         Object.keys(this.state.appList).map((app, index) => {
                             const info = this.state.appList[app]
-                            const appName = info.appDirName.substring(info.appDirName.lastIndexOf('/') + 1)
+                            const appName =
+                                info.appDirName.substring(info.appDirName.lastIndexOf('/') + 1)
                             return (
                                 <div key={index} style={{
                                     width: '100%',
@@ -97,9 +138,9 @@ class AppList extends React.Component {
                                     alignItems: 'center',
                                     overflow: 'hidden'
                                 }} className={`${info.appDirName === this.state.activeAppDirName ? 'active-app animate__animated animate__pulse' : ''}`}
-                                title={`${info.appDirName}`} onClick={() => {
-                                    this.selectApp(info)
-                                }}>
+                                    title={`${info.appDirName}`} onClick={() => {
+                                        this.selectApp(info)
+                                    }}>
                                     <img src={info.icon} style={{
                                         width: '25px',
                                         height: '25px',
@@ -119,25 +160,81 @@ class AppList extends React.Component {
                     }
                 </div>
 
-                {/* 操作栏 */}
-
+                {/* 模板选择栏 */}
                 <div style={{
-                    width: '100%',
+                    width: 'calc(100% - 4px)',
+                }} onMouseLeave={() => {
+                    this.hideTemplateSelectMenu()
+                }}>
+                    {
+                        ['showing', 'show', 'hidding'].includes(this.state.templateListStatus) ? (
+                            <div style={{
+                                width: '100%',
+                                // borderTop: '1px solid #f30ba4 ',
+                                // borderRight: '1px solid #f30ba4 ',
+                                borderRadius: '0 5px 0 0 ',
+                                boxShadow: '1px -1px 4px #f30ba4'
+                            }} className={`${['show', 'showing'].includes(this.state.templateListStatus) ? 
+                                'template-list-show' : 'template-list-hide'}`}>
+                                <div style={{
+                                    fontSize: '14px',
+                                    padding: '10px 5px 5px 5px',
+                                }}>
+                                    {this.state.i18n['capp-chooseYourTemplate']}
+                                </div>
+
+                                <div style={{
+                                    width: '100%',
+                                    height: '150px',
+                                    overflowX: 'hidden',
+                                    overflowY: 'auto'
+                                }} className={`better-scroll`}>
+                                    {
+                                        this.state.templates.map((item, index) => {
+                                            return (
+                                                <div style={{
+                                                    fontWeight: 'bold'
+                                                }}>
+                                                    <CusMenuItem onClick={() => {
+                                                        this.hideTemplateSelectMenu()
+                                                        this.createAppByTemplate(item)
+                                                    }}>{item.name}</CusMenuItem>
+                                                </div>
+                                            )
+                                        })
+                                    }
+                                </div>
+                            </div>
+                        ) : null
+                    }
+                </div>
+
+
+                {/* 操作栏 */}
+                <div style={{
+                    width: 'calc(100% - 4px)',
                     height: '30px',
                     backgroundColor: '',
                     boxShadow: '0px -1px 4px #ccc',
                     display: 'flex',
                     justifyContent: 'center',
                     alignItems: 'center',
-                    fontSize: '18px',
+                    fontSize: '12px',
                     color: '#666',
-                    cursor: 'pointer'
-                }} onClick={async () => {
-                    await fm.controller.createApp({template: 'demo'})
+                    cursor: 'pointer',
+                }} onClick={() => {
+                    this.showTemplateSelectMenu()
                 }}>
-                    +
+                    {this.state.i18n['capp-createApp']}
                 </div>
             </div>
         )
     }
 }
+
+// 自定义组件样式
+const CusMenuItem = styled(MenuItem)(({ theme }) => ({
+    fontSize: '14px',
+    color: '#666',
+    fontFamily: 'JiangCheng'
+}));
--- __core/factory/components/FactoryPanel.jsx
+++ __core/factory/components/FactoryPanel.jsx
@@ -1,5 +1,6 @@
 const { DragBar, WindowFrame, GlobalHandler } = window.fmComponents
-const { Button, TextField } = window.MaterialUI
+const { Button, TextField, Switch, styled, alpha } = window.MaterialUI
+const { pink } = window.MaterialUI.colors
 
 class FactoryPanel extends React.Component {
     constructor(p) {
@@ -23,6 +24,7 @@ class FactoryPanel extends React.Component {
 
         this.updateAppList()
         fm.on.staticFileChange(async () => {
+            this.updateAppInfo()
             this.updateAppList()
             this.checkAppName(this.state.editingAppDirName)
         })
@@ -34,6 +36,15 @@ class FactoryPanel extends React.Component {
         }
     }
 
+    // 与基座同步配置
+    // 同步配置需要传入配置参数，由 onStaticFile 来触发app配置更新，再更新state
+    async syncConfig(configJSON) {
+        await fm.controller.setAppConfig({
+            appDirName: this.state.currentAppInfo.appDirName,
+            configJSON: configJSON
+        })
+    }
+
     // 更新app列表，用来做排重
     async updateAppList() {
         const appList = (await fm.controller.getAppList()).appList
@@ -41,13 +52,6 @@ class FactoryPanel extends React.Component {
             appList: appList
         })
     }
-
-    // async syncConfig() {
-    //     await fm.controller.setAppConfig({
-    //         appDirName: this.state.currentAppInfo.appDirName,
-    //         configJSON: this.state.configJSON
-    //     })
-    // }
 
     // 更新 app 配置
     async updateAppInfo() {
@@ -64,16 +68,13 @@ class FactoryPanel extends React.Component {
 
     // 检查重名
     async checkAppName(inputAppName) {
-        if(!this.state.isEditingAppDirName) return 
+        if (!this.state.isEditingAppDirName) return
         // 检查是否出错
         const appDirPrefix = this.state.currentAppInfo.appDirName.substring(
             0, this.state.currentAppInfo.appDirName.lastIndexOf('/')
         )
         const newAppDirName = appDirPrefix + '/' + inputAppName
         let error = false
-        if (inputAppName === '' || !inputAppName) {
-            error = true
-        }
         for (let key in this.state.appList) {
             if (key === newAppDirName) { // 唯一索引不能重复
                 error = true
@@ -97,15 +98,19 @@ class FactoryPanel extends React.Component {
     async openAppNameEditFrame() {
         this.setState({
             isEditingAppDirName: true,
-            editingAppDirNameError: true,
-            editingAppDirName: this.state.currentAppInfo.appDirName.substring(
-                this.state.currentAppInfo.appDirName.lastIndexOf('/') + 1
-            )
+            editingAppDirNameError: false,
+            editingAppDirName: ''
         })
     }
 
     // 提交 app 名称更改
     async submitAppNameChange(newAppName) {
+        if (!newAppName || newAppName === '') {
+            await fm.dialog.showErrorBox({
+                message: this.state.i18n['capp-newAppNameError']
+            })
+            return
+        }
         const res = await fm.controller.setAppName({
             appDirName: this.state.currentAppInfo.appDirName,
             newAppName: newAppName
@@ -118,6 +123,31 @@ class FactoryPanel extends React.Component {
         await fm.dialog.showErrorBox({
             message: res.message
         })
+    }
+
+    // 删除APP
+    async deleteApp(appDirName) {
+        const dialogRes = await fm.dialog.showMessageBox({
+            type: 'warning',
+            title: this.state.i18n['capp-deleteApp'],
+            message: this.state.i18n['capp-deleteAppMsg'] + appDirName,
+            buttons: ['OK', 'Cancel']
+        })
+
+        if (dialogRes.result !== 0) return
+
+        const res = await fm.controller.deleteApp({
+            appDirName: appDirName
+        })
+
+        if (res.status !== 2000) {
+            await fm.dialog.showErrorBox({
+                message: res.message
+            })
+            return
+        }
+
+        location.reload()
     }
 
     render() {
@@ -152,6 +182,7 @@ class FactoryPanel extends React.Component {
                     <div style={{
                         width: '100%',
                         display: 'flex',
+                        flexWrap: 'wrap',
                         padding: '20px 20px 20px 20px'
                     }}>
                         {/* 图标 */}
@@ -164,7 +195,7 @@ class FactoryPanel extends React.Component {
                             alignItems: 'center',
                             alignContent: 'center',
                             // background: '#e1e'
-                            border: '1px solid #eee',
+                            // border: '1px solid #eee',
                             borderRadius: '10px 0px 10px 0px',
                             boxShadow: '2px 2px 4px #f30ba4, -2px -2px 4px #feea83'
                         }} className={`hvr-grow`}>
@@ -172,7 +203,9 @@ class FactoryPanel extends React.Component {
                                 width: '90px',
                                 height: '90px',
                                 userSelect: 'none',
-                            }} src={this.state.currentAppInfo.icon} title={`${this.state.i18n['capp-setIconTips']}`} />
+                            }}
+                                src={this.state.currentAppInfo.icon}
+                                title={`${this.state.i18n['capp-setIconTips']}`} />
                         </div>
 
                         {/* app信息 */}
@@ -180,7 +213,6 @@ class FactoryPanel extends React.Component {
                             width: '60%',
                             height: '100px',
                             // background: '#e1e',
-                            padding: '0 20px 0 20px',
                             display: 'flex',
                             flexDirection: 'column',
                             justifyContent: 'flex-start',
@@ -199,6 +231,7 @@ class FactoryPanel extends React.Component {
                                         flexDirection: 'row',
                                         alignContent: 'center',
                                         alignItems: 'center',
+                                        paddingLeft: '20px',
                                         textShadow: '0px 0px 1px #f30ba4, 0px 0px 10px #feea83, 0px 0px 10px #feea83'
                                     }}>
                                         <div style={{
@@ -215,12 +248,12 @@ class FactoryPanel extends React.Component {
                                             display: 'flex',
                                             alignItems: 'center',
                                             marginLeft: '10px',
-                                            cursor: 'pointer'
+                                            cursor: 'pointer',
                                         }} onClick={() => {
                                             this.openAppNameEditFrame()
-                                        }}>
-                                            <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-                                                <path stroke="#666" d="M20 21H4L8 19L18.4056 6.70242C18.7416 6.30539 18.7171 5.71713 18.3494 5.34937L16.7685 3.76848C16.3548 3.3548 15.6759 3.38304 15.298 3.82965L5 16L4 19" stroke-linecap="round" stroke-linejoin="round" />
+                                        }} className={`hvr-skew-forward`}>
+                                            <svg width="20" height="20" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                                <path stroke="#999" d="M20 21H4L8 19L18.4056 6.70242C18.7416 6.30539 18.7171 5.71713 18.3494 5.34937L16.7685 3.76848C16.3548 3.3548 15.6759 3.38304 15.298 3.82965L5 16L4 19" stroke-linecap="round" stroke-linejoin="round" />
                                             </svg>
                                         </div>
                                     </div>
@@ -272,11 +305,11 @@ class FactoryPanel extends React.Component {
                                                             this.state.editingAppDirName
                                                         )
                                                     }} title={`${this.state.i18n['capp-confirm']}`}>
-                                                        <svg version="1.1" id="icons" xmlns="http://www.w3.org/2000/svg"  x="0px" y="0px"
+                                                        <svg version="1.1" id="icons" xmlns="http://www.w3.org/2000/svg" x="0px" y="0px"
                                                             width="24" height="24" viewBox="0 0 41.92 41.92" enable-background="new 0 0 41.92 41.92" space="preserve">
                                                             <g id="Icon_52_">
                                                                 <circle fill="none" stroke="#66db38" stroke-width="3.92" stroke-linecap="round" stroke-miterlimit="10" cx="20.96" cy="20.96" r="19" />
-                                                                <polyline fill="none" stroke="#66db38" stroke-width="4" stroke-linecap="round" stroke-linejoin="round" stroke-miterlimit="10" points="13.091,20.338 18.483,25.87 29.091,15.87 	"/>
+                                                                <polyline fill="none" stroke="#66db38" stroke-width="4" stroke-linecap="round" stroke-linejoin="round" stroke-miterlimit="10" points="13.091,20.338 18.483,25.87 29.091,15.87 	" />
                                                             </g>
                                                         </svg>
 
@@ -297,17 +330,20 @@ class FactoryPanel extends React.Component {
                                 ) : null
                             }
 
+                            {/* 源码目录 */}
                             <div style={{
                                 fontSize: '12px',
                                 color: '#666',
-                                marginTop: '10px',
-                                cursor: 'pointer'
+                                marginTop: '5px',
+                                cursor: 'pointer',
+                                userSelect: 'none',
+                                paddingLeft: '20px',
                             }} title={`${this.state.i18n['capp-openExplorer']}`} onClick={async () => {
                                 await fm.controller.openExplorer({
                                     appDirName: this.state.currentAppInfo.appDirName
                                 })
                             }} className={`hvr-skew-forward`}>
-                                源码目录：{this.state.currentAppInfo.appDirName}
+                                {this.state.i18n['capp-sourceCodeDir']} {this.state.currentAppInfo.appDirName}
                             </div>
                         </div>
 
@@ -317,12 +353,12 @@ class FactoryPanel extends React.Component {
                             display: 'flex',
                             flexDirection: 'row',
                             alignItems: 'center',
-                            justifyContent: 'center'
+                            justifyContent: 'center',
                         }}>
                             {/* 按钮1 */}
                             <div style={{
                                 width: '50%',
-                                textAlign: 'center',
+                                textAlign: 'left',
                                 cursor: 'pointer'
                             }} onClick={async () => {
                                 await fm.openApp({
@@ -336,25 +372,54 @@ class FactoryPanel extends React.Component {
                             {/* 按钮2 */}
                             <div style={{
                                 width: '50%',
-                                textAlign: 'center',
+                                textAlign: 'left',
                                 cursor: 'pointer'
                             }} onClick={async () => {
                                 await fm.controller.closeApp({
                                     appDirName: this.state.currentAppInfo.appDirName
                                 })
-                            }} className={`hvr-wobble-skew`} title={`${this.state.i18n['capp-closeAllThisApp']}`}>
+                                await this.deleteApp(this.state.currentAppInfo.appDirName)
+                            }} className={`hvr-wobble-skew`} title={`${this.state.i18n['capp-deleteAppTitle']}`}>
                                 <svg width="33" height="33" viewBox="0 0 24 24" fill="#f08b06 " xmlns="http://www.w3.org/2000/svg">
                                     <path stroke="#f00606" d="M9.8788 7.05025L7.75748 4.92893C6.97643 4.14788 5.7101 4.14788 4.92905 4.92893C4.148 5.70997 4.148 6.9763 4.92905 7.75735L9.17169 12L4.92905 16.2426C4.148 17.0237 4.148 18.29 4.92905 19.0711C5.7101 19.8521 6.97643 19.8521 7.75747 19.0711L12.0001 14.8284L16.2428 19.0711C17.0238 19.8521 18.2901 19.8521 19.0712 19.0711C19.8522 18.29 19.8522 17.0237 19.0712 16.2426L14.8285 12L19.0712 7.75735C19.8522 6.97631 19.8522 5.70998 19.0712 4.92893C18.2901 4.14788 17.0238 4.14788 16.2428 4.92893L12.0001 9.17157" stroke-linecap="round" stroke-linejoin="round" />
                                 </svg>
                             </div>
                         </div>
 
-                        {/* factory panel {this.state.currentAppInfo && this.state.currentAppInfo.appDirName}
-                        <button onClick={async () => {
-                            await fm.controller.closeApp({
-                                appDirName: "apps/github.com.stevewooo/chat"
-                            })
-                        }}>close chat</button> */}
+                        {/* 配置列表 */}
+                        <div style={{
+                            width: '100%',
+                            display: 'flex',
+                            flexDirection: 'column',
+                            overflow: 'hidden',
+                            marginTop: '20px',
+                        }}>
+                            {/* {JSON.stringify(this.state.configJSON)} */}
+                            {/* 控制台配置 */}
+                            <div style={{
+                                width: '50%',
+                                display: 'flex',
+                                flexDirection: 'row',
+                                flexWrap: 'wrap',
+                                alignItems: 'center'
+                            }}>
+                                <div style={{
+                                }} className={`configure-menu-item `}>
+                                    {this.state.i18n['capp-openDevTools']}
+                                </div>
+                                <div style={{
+                                }} className={`configure-menu-item `}>
+                                    <ConfigureSwitch checked={
+                                        this.state.configJSON && this.state.configJSON.configure.devTools === true
+                                    } onChange={async (e) => {
+                                        const configJSON = this.state.configJSON;
+                                        configJSON.configure.devTools = e.target.checked;
+                                        this.syncConfig(configJSON)
+                                    }} />
+                                </div>
+
+                            </div>
+                        </div>
                     </div>
                 )}
             </div>
@@ -362,3 +427,15 @@ class FactoryPanel extends React.Component {
     }
 }
 
+// 自定义组件样式
+const ConfigureSwitch = styled(Switch)(({ theme }) => ({
+    '& .MuiSwitch-switchBase.Mui-checked': {
+        color: pink[600],
+        '&:hover': {
+            backgroundColor: alpha(pink[600], theme.palette.action.hoverOpacity),
+        },
+    },
+    '& .MuiSwitch-switchBase.Mui-checked + .MuiSwitch-track': {
+        backgroundColor: pink[600],
+    },
+}));
