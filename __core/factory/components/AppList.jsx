7000a3b8d9d23d3fdc7883e52baf3915d8386108
Live fish is worried, Dead Fish is happy <461437874@qq.com>
2024-01-19 14:32:53 +0800

    [优化]优化工坊体验

--- __core/factory/components/AppList.jsx
+++ __core/factory/components/AppList.jsx
@@ -28,8 +28,8 @@ class AppList extends React.Component {
 
     async componentDidUpdate(prevProps) {
         if (this.props.currentAppInfo !== prevProps.currentAppInfo) {
-            if (this.props.currentAppInfo === null ) {
-                return 
+            if (this.props.currentAppInfo === null) {
+                return
             }
             this.setState({
                 activeAppDirName: this.props.currentAppInfo.appDirName
@@ -60,6 +60,13 @@ class AppList extends React.Component {
 
     async selectApp(appInfo) {
         // console.log(appInfo)
+        if (appInfo == null) {
+            this.props.selectApp && this.props.selectApp(null)
+            this.setState({
+                activeAppDirName: ''
+            })
+            return
+        }
         this.setState({
             activeAppDirName: appInfo.appDirName
         })
@@ -117,6 +124,33 @@ class AppList extends React.Component {
         })
     }
 
+    // 删除APP
+    async deleteApp(appDirName) {
+        const dialogRes = await fm.dialog.showMessageBox({
+            type: 'warning',
+            title: this.state.i18n['capp-deleteApp'],
+            message: this.state.i18n['capp-deleteAppMsg'] + appDirName,
+            buttons: ['OK', 'Cancel']
+        })
+
+        if (dialogRes.result !== 0) return
+
+        this.selectApp(null)
+        const res = await fm.controller.deleteApp({
+            appDirName: appDirName
+        })
+        // console.log(res)
+        if (res.status !== 2000) {
+            await fm.dialog.showErrorBox({
+                message: res.message
+            })
+            return
+        }
+
+        // location.reload()
+        this.updateList()
+    }
+
     render() {
         return (
             <div style={{
@@ -168,6 +202,7 @@ class AppList extends React.Component {
                                 <div key={index} style={{
                                     width: '100%',
                                     height: '35px',
+                                    paddingLeft: '3px',
                                     fontSize: '14px',
                                     display: 'flex',
                                     flexWrap: 'nowrap',
@@ -178,18 +213,47 @@ class AppList extends React.Component {
                                     title={`${info.appDirName}`} onClick={() => {
                                         this.selectApp(info)
                                     }}>
-                                    <img src={info.icon} style={{
-                                        width: '25px',
-                                        height: '25px',
-                                        marginLeft: '5px'
-                                    }} />
                                     <div style={{
-                                        paddingLeft: '10px',
-                                        height: '35px',
-                                        lineHeight: '35px',
-                                        flexGrow: 1,
-                                    }} className={`hvr-forward`}>
-                                        {appName}
+                                        width: '10%',
+                                        lineHeight: '25px',
+                                        display: 'flex',
+                                        alignItems: 'center',
+                                        alignContent: 'center'
+                                    }}>
+                                        <img src={info.icon} style={{
+                                            width: '25px',
+                                            height: '25px',
+                                        }} />
+                                    </div>
+                                    <div style={{
+                                        width: '80%',
+                                        overflow: 'hidden',
+                                    }}>
+
+                                        <div style={{
+                                            paddingLeft: '10px',
+                                            height: '35px',
+                                            lineHeight: '35px',
+                                            flexGrow: 1,
+
+                                        }} className={`hvr-forward`}>
+                                            {appName}
+                                        </div>
+                                    </div>
+
+                                    <div style={{
+                                        width: '10%',
+                                        cursor: 'pointer',
+                                        opacity: 0.7
+                                    }} onClick={async () => {
+                                        await fm.controller.closeApp({
+                                            appDirName: info.appDirName
+                                        })
+                                        await this.deleteApp(info.appDirName)
+                                    }} className={`hvr-wobble-skew appListDeleteTag`} title={`${this.state.i18n['capp-deleteAppTitle']}`}>
+                                        <svg width="16" height="16" viewBox="0 0 24 24" fill="#f08b06 " xmlns="http://www.w3.org/2000/svg">
+                                            <path stroke="#f00606" d="M9.8788 7.05025L7.75748 4.92893C6.97643 4.14788 5.7101 4.14788 4.92905 4.92893C4.148 5.70997 4.148 6.9763 4.92905 7.75735L9.17169 12L4.92905 16.2426C4.148 17.0237 4.148 18.29 4.92905 19.0711C5.7101 19.8521 6.97643 19.8521 7.75747 19.0711L12.0001 14.8284L16.2428 19.0711C17.0238 19.8521 18.2901 19.8521 19.0712 19.0711C19.8522 18.29 19.8522 17.0237 19.0712 16.2426L14.8285 12L19.0712 7.75735C19.8522 6.97631 19.8522 5.70998 19.0712 4.92893C18.2901 4.14788 17.0238 4.14788 16.2428 4.92893L12.0001 9.17157" stroke-linecap="round" stroke-linejoin="round" />
+                                        </svg>
                                     </div>
                                 </div>
                             )
@@ -202,7 +266,7 @@ class AppList extends React.Component {
                     width: 'calc(100% - 4px)',
                 }} onMouseLeave={() => {
                     if (['show', 'showing'].includes(this.state.templateListStatus)) {
-                        
+
                         this.hideTemplateSelectMenu()
                     }
                 }}>
